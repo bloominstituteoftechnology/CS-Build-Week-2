5ea68b2198e624ddb1560259005feb7ba5f35bea
Josh Akeman <joshakeman@gmail.com>
2019-08-26 18:01:43 -0400

    still tweaking

--- explorer/src/App.js
+++ explorer/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
 
-import { Stack, Queue } from './helpers'
+import { Stack, Queue, getRandomDirection, getRandomInt } from './helpers'
 
 class App extends React.Component {
 
@@ -9,22 +10,167 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      state: 'state'
+      state: 'state',
+      currentRoom: {}
     }
   }
 
   componentDidMount() {
-    let qoo = new Queue
+    let qoo = new Queue()
     this.setState({
       queue: qoo
     })
   }
- 
 
+  makePath = () => {
+
+    console.log("firing button")
+
+    let graph = {}
+    let path = []
+    console.log(graph.length, path)
+    // Object.keys(myArray).length
+
+    while (Object.keys(graph).length < 500) {
+        console.log(`graph is this long ${graph.length}`)
+        localStorage.setItem('map', JSON.stringify(graph))
+
+        let notExplored = []
+
+        const exits = this.state.currentRoom.exits
+
+        const current = this.state.currentRoom.room_id
+
+        if (!(current in graph)) {
+            graph[current] = {}
+            for (var x in exits) {
+                graph[current][exits[x]] = '?'
+            }
+        }
+        
+        for (var room in graph[current]) {
+            console.log(room)
+            if (graph[current][room] == "?") {
+                notExplored.push(room)
+            } 
+          }
+
+        if (notExplored.length > 0) {
+            let nextMove = getRandomDirection(notExplored)
+
+            this.movePlayer(nextMove)
+
+            graph[current][nextMove] = this.state.currentRoom.room_id
+            path.push(nextMove)
+            notExplored = []
+        } else {
+            let q = new Queue()
+            q.enqueue([current])
+
+            let checked = []
+            let returnPath = []
+
+            while (q.size() > 0) {
+                let checkPath = q.dequeue()
+                let v = checkPath.slice(-1)[0]
+
+
+                if (!(v in returnPath)) {
+                    let exists = Object.keys(graph[v]).some(function(k) {
+                        return returnPath[k] === "?";
+                    });
+                    if (exists == true) {
+                        returnPath = checkPath
+                        break
+                    }
+                    returnPath.push(v)
+                    for (var way in graph[v]) {
+                        let new_path = [...checkPath]
+                        new_path.push(graph[v][way])
+                        q.enqueue(new_path)
+                    }
+                }
+            }
+
+            let steps = []
+
+            for (var i = 0; i < returnPath.length - 1; i++) { 
+                for (var direction in graph[returnPath[i]]) {
+                    if (graph[returnPath[i]][direction] == returnPath[i + 1]) {
+                        steps.push(direction)
+                    }
+                }
+                path = path.concat(steps)
+                for (var step in steps) {
+                    this.movePlayer(step)
+                }
+              }
+            
+        }
+    }
+
+    return path
+}
+ 
+movePlayer = (direction) => {
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token 314369d39e5bdc8abb7f7c40689cb34de2d56669'
+  }
+
+  const data = {
+    direction: 'e'
+  }
+  
+  axios(
+    {
+      method: 'post', //you can set what request you want to be
+      url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
+      data: data,
+      headers: {
+        Authorization: 'Token 314369d39e5bdc8abb7f7c40689cb34de2d56669'
+      }
+    }
+  )
+    .then((res) => {
+      console.log(res)
+      this.setState({
+        currentRoom: res.data
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+startGame = () => {
+
+  const headers = {
+    'Authorization': 'Token 314369d39e5bdc8abb7f7c40689cb34de2d56669'
+  }
+  
+  axios
+    .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', {headers})
+    .then((res) => {
+      console.log(res)
+      this.setState({
+        currentRoom: res.data
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
   render() {
     return (
+      <>
       <h2>Explorer</h2>
+      <button onClick={this.startGame}>Start Game</button>
+      <button onClick={this.movePlayer}>Move Player</button>
+      <button onClick={this.makePath}>Start Exploring</button>
+      </>
     );
   }
 }
--- explorer/src/helpers.js
+++ explorer/src/helpers.js
@@ -28,15 +28,124 @@ export class Queue {
     enqueue(value) {
         this.queue.push(value)
     }
-    pop() {
+    dequeue() {
         if (this.size() > 0) {
-            return this.stack.shift()
+            return this.queue.shift()
         }
         else {
             return null
         }
     }
     size() {
-        return this.stack.length
+        return this.queue.length
     }
 }
+
+// export const makePath = (player, room = {}) => {
+
+//     let graph = {}
+//     let path = []
+
+//     while (graph.length < 500) {
+//         console.log(`graph is this long ${graph.length}`)
+
+//         let notExplored = []
+
+//         const exits = room.exits
+
+//         const current = room.room_id
+
+//         if (!(current in graph)) {
+//             graph[current] = {}
+//             for (var x in exits) {
+//                 graph[current][exits[x]] = '?'
+//             }
+//         }
+        
+//         for (var room in graph[current]) {
+//             console.log(room)
+//             if (room == "?") {
+//                 notExplored.push(direction)
+//             } 
+//           }
+
+//         if (notExplored.length > 0) {
+//             let nextMove = getRandomDirection(notExplored)
+
+//             //make axios call with next move, set new room
+//             //on state 
+
+//             graph[current][nextMove] = room.id
+//             path.push(nextMove)
+//             notExplored = []
+//         } else {
+//             let q = new Queue()
+//             q.enqueue([current])
+
+//             let checked = []
+//             let returnPath = []
+
+//             while (q.size() > 0) {
+//                 checkPath = q.dequeue()
+//                 let v = checkPath[-1]
+
+
+//                 if (!(v in returnPath)) {
+//                     let exists = Object.keys(graph[v]).some(function(k) {
+//                         return returnPath[k] === "?";
+//                     });
+//                     if (exists == true) {
+//                         returnPath = checkPath
+//                         break
+//                     }
+//                     returnPath.append(v)
+//                     for (way in graph[v]) {
+//                         let new_path = [...checkPath]
+//                         new_path.push(graph[v][way])
+//                         q.enqueue(new_path)
+//                     }
+//                 }
+//             }
+
+//             let steps = []
+
+//             for (i = 0; i < returnPath.length - 1; i++) { 
+//                 for (direction in graph[returnPath[i]]) {
+//                     if (graph[returnPath[i]][direction] == returnPath[i + 1]) {
+//                         steps.push(direction)
+//                     }
+//                 }
+//                 path = path.concat(steps)
+//                 for (step in steps) {
+//                     //axios call to have player move
+//                 }
+//               }
+            
+//         }
+//     }
+
+//     return path
+// }
+
+export const getRandomDirection = (exits) => {
+    const chosenExit = exits[getRandomInt(0, exits.length)]
+    return chosenExit
+}
+
+export function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+    
+
+// {
+//     "room_id": 0,
+//     "title": "A Dark Room",
+//     "description": "You cannot see anything.",
+//     "coordinates": "(60,60)",
+//     "exits": ["n", "s", "e", "w"],
+//     "cooldown": 1.0,
+//     "errors": [],
+//     "messages": []
+//   }
