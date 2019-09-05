782ce8fe11a5676f95d77eaff7889f820efa42a4
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-09-04 22:36:01 -0600

    Deploy Test

--- treasure-map/src/App.js
+++ treasure-map/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn German
         </a>
       </header>
     </div>
