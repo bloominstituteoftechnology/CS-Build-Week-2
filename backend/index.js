8b24c263ad4b8c40575cbaf6bba9389624852421
BryceEvans <evans.bryce@gmail.com>
2019-09-09 22:12:24 -0600

    a few endopoints added.

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,28 @@ const PORT = 5050;
 
 const getUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/';
 const moveUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/';
+// // Uncomment These as you need them...
+const takeUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/take/';
+const dropUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/';
+const sellUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/';
+// const statusUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/status/';
+const examineUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/';
+// const wearUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/wear/';
+// const changeNameUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/';
+// const prayUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/pray/';
+// const flyUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/fly/';
+// const dashUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/dash/';
+// const carryUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/carry/';
+// const receiveUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/receive/';
+// const mineUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/bc/mine/';
+// const proofUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/';
+// const balanceUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/bc/get_balance/';
+// const transmorgrifyUrl = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/transmogrify/';
 const params = {
-  TOKEN: '65ef3fd1d9226f97f50a440cb4dd09b64e0d6a8c'
+  // // K-token
+  // TOKEN: '65ef3fd1d9226f97f50a440cb4dd09b64e0d6a8c'
+  // // B-token
+  TOKEN: '75578be1cf6136d88fb6b170e43b7da71dea5f84'
 };
 
 async function getRoom() {
@@ -73,8 +93,135 @@ async function move() {
     .catch(err => console.log(err));
 }
 
-//getRoom();
-move();
+async function take() {
+  const config = {
+    method: 'post',
+    url: takeUrl,
+    headers: {
+      Authorization: `Token ${params.TOKEN}`
+    },
+    body: {
+      name: 'treasure'
+    }
+  };
+  await axios({
+    method: config.method,
+    url: takeUrl,
+    data: config.body,
+    headers: config.headers
+  })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}
+
+async function drop() {
+  const config = {
+    method: 'post',
+    url: dropUrl,
+    headers: {
+      Authorization: `Token ${params.TOKEN}`
+    },
+    body: {
+      name: 'treasure'
+    }
+  };
+  await axios({
+    method: config.method,
+    url: dropUrl,
+    data: config.body,
+    headers: config.headers
+  })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}
+
+async function sell() {
+  const config = {
+    method: 'post',
+    url: sellUrl,
+    headers: {
+      Authorization: `Token ${params.TOKEN}`
+    },
+    body: {
+      name: 'treasure'
+    }
+  };
+  await axios({
+    method: config.method,
+    url: sellUrl,
+    data: config.body,
+    headers: config.headers
+  })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}
+
+async function confirmSell() {
+  const config = {
+    method: 'post',
+    url: sellUrl,
+    headers: {
+      Authorization: `Token ${params.TOKEN}`
+    },
+    body: {
+      name: 'treasure',
+      confirm: 'yes'
+    }
+  };
+  await axios({
+    method: config.method,
+    url: sellUrl,
+    data: config.body,
+    headers: config.headers
+  })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}
+
+// status function here
+
+async function examine() {
+  const config = {
+    method: 'post',
+    url: examineUrl,
+    headers: {
+      Authorization: `Token ${params.TOKEN}`
+    },
+    body: {
+      name: 'treasure'
+    }
+  };
+  await axios({
+    method: config.method,
+    url: examineUrl,
+    data: config.body,
+    headers: config.headers
+  })
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}
+
+getRoom();
+// move();
+// take();
+// drop();
+// sell();
+// confirmSell();
+// status();
+// examine();
+
+
+
 
 server.listen(PORT, () => {
   console.log(`Server is listening on Port ${PORT}`);
