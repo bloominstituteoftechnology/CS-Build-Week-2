37293efb4b51c4e1b018e60233ad72bbe72861b5
micahjones13 <micahjones13@gmail.com>
2019-11-19 16:16:06 -0700

    map fully mapped

--- explore.py
+++ explore.py
@@ -1,8 +1,8 @@
 from utils import *
 
 mappy = mapper()
-#mappy.accumulate=False   #set pick up items to false
-
+# mappy.accumulate=False   #set pick up items to false
+# mappy.import_text_map = False
 mappy.create_starting_map()
 
-mappy.explore_random(50)
+mappy.explore_random(450)
