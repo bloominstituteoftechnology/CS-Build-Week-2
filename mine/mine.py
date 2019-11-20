ec6d0e11b5c1a23b809369f7dae313076b57c956
quansenB <inaki.breinbauer@gmail.com>
2019-11-20 12:33:54 +0100

    basic mining algo

--- mine/mine.py
+++ mine/mine.py
@@ -1,41 +1,58 @@
 import requests
+""" import schedule """
 import time
 import os
 import json
 import random
+import hashlib
 from dotenv import load_dotenv
 load_dotenv()
 
 
 lastProof = 0
-difficulty = 0
+difficulty = 0 
+leadingZeros = "0"
 
 def init():
     res = requests.get('https://lambda-treasure-hunt.herokuapp.com/api/bc/last_proof/',
                        headers={'Authorization': str(os.getenv("authToken"))})
 
     if res:
+        global lastProof
+        global difficulty
+        global leadingZeros
         res = res.json()
         lastProof = res['proof']
         difficulty = res['difficulty']
+        leadingZeros = "0" * difficulty
 
         time.sleep(res['cooldown'])
 
 def mine():
-    rndNumber = random.randrange(2, 1001)
-    proposedProof = hash(lastProof, rndNumber)
+    global lastProof
+    global difficulty
+    global leadingZeros
 
-    while proof(proposedProof) is not True:
-        rndNumber += random.randrange(2, 1001)
-        proposedProof = hash(lastProof, rndNumber)
+    guess = random.randrange(3274, 6821)
+    encoded = f'{lastProof}{guess}'.encode()
+    hashed = hashlib.sha256(encoded).hexdigest()
 
+    print(hashed[:difficulty], leadingZeros)
+
+    while hashed[:difficulty] is leadingZeros:
+        guess += random.randrange(3274, 6821)
+        encoded = f'{lastProof}{guess}'.encode()
+        hashed = hashlib.sha256(encoded).hexdigest()
+
+    print(hashed)
     res = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/bc/mine/',
                         headers={'Authorization': str(os.getenv('authToken'))},
-                        json={'proof': proposedProof}
+                        json={'proof': guess}
                         )
     print(res)
     if res:
         res = res.json()
+        print(res)
 
         time.sleep(res['cooldown'])
 
@@ -51,6 +68,7 @@ def proof(proposedProof):
         return False
 
 
+""" schedule.every(2).seconds.do(init) """
 init()
 while True:
     mine()
