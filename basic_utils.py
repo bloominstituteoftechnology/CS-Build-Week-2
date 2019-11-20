7d30eeb0ae95c661da3ebe41cf391463e274e754
micahjones13 <micahjones13@gmail.com>
2019-11-19 19:15:02 -0700

    can now find and sell treasure

--- basic_utils.py
+++ basic_utils.py
@@ -1,48 +1,62 @@
 class Queue():
     def __init__(self):
         self.queue = []
+
+    def __repr__(self):
+        return f'{self.queue}'
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
         if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
+
     def size(self):
         return len(self.queue)
+
 
 class Stack():
     def __init__(self):
         self.stack = []
+
     def push(self, value):
         self.stack.append(value)
+
     def pop(self):
         if self.size() > 0:
             return self.stack.pop()
         else:
             return None
+
     def size(self):
         return len(self.stack)
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
         if vertex not in self.vertices.keys():
-          self.vertices[vertex] = set()
+            self.vertices[vertex] = set()
         else:
-          pass
+            pass
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
         if v1 and v2 in self.vertices.keys():
-         #self.vertices[v2].add(v1)
-          self.vertices[v1].add(v2)
+         # self.vertices[v2].add(v1)
+            self.vertices[v1].add(v2)
 
     def bft(self, starting_vertex):
         """
@@ -51,23 +65,21 @@ class Graph:
         """
         ret_list = []
         if starting_vertex is None:
-          return None
+            return None
         my_q = Queue()
         visited = [starting_vertex]
         my_q.enqueue(starting_vertex)
         while len(my_q.queue) > 0:
-          point = my_q.queue[0]
-          joins = self.vertices[point]
-          for j in joins:
-            if j not in visited:
-              my_q.enqueue(j)
-              visited.append(j)
-          #print(my_q.dequeue())
-          ret = my_q.dequeue()
-          ret_list.append(ret)
+            point = my_q.queue[0]
+            joins = self.vertices[point]
+            for j in joins:
+                if j not in visited:
+                    my_q.enqueue(j)
+                    visited.append(j)
+            # print(my_q.dequeue())
+            ret = my_q.dequeue()
+            ret_list.append(ret)
         return ret_list
-          
-
 
     def dft(self, starting_vertex, chooser=None):
         """
@@ -76,30 +88,29 @@ class Graph:
         """
         ret_list = []
         if starting_vertex is None:
-          return None
+            return None
         my_s = Stack()
         visited = [starting_vertex]
         my_s.push(starting_vertex)
         while len(my_s.stack) > 0:
-          point = my_s.stack[-1]
-          joins = self.vertices[point]
-          r = my_s.pop()   ##new code
-          ret_list.append(r)  ##new code
-          #print(r)  ##changed to r from pop
-          if chooser is None:
-            pass
-          elif chooser == 'random':
-            joins = random.sample(joins,len(joins))
-          elif chooser == 'shortest':
-            joins = find_longest_clique(point,self,visited)
-          for j in joins:
-            if j not in visited:
-              my_s.push(j)
-              visited.append(j)
+            point = my_s.stack[-1]
+            joins = self.vertices[point]
+            r = my_s.pop()  # new code
+            ret_list.append(r)  # new code
+            # print(r)  ##changed to r from pop
+            if chooser is None:
+                pass
+            elif chooser == 'random':
+                joins = random.sample(joins, len(joins))
+            elif chooser == 'shortest':
+                joins = find_longest_clique(point, self, visited)
+            for j in joins:
+                if j not in visited:
+                    my_s.push(j)
+                    visited.append(j)
         return ret_list
 
-
-    def dft_recursive(self, starting_vertex, visited = []):
+    def dft_recursive(self, starting_vertex, visited=[]):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
@@ -109,14 +120,12 @@ class Graph:
         visited.append(starting_vertex)
         joins = self.vertices[starting_vertex]
         if joins is None:
-          return None
+            return None
         for j in joins:
-          if j in visited:
-            pass
-          else:
-            self.dft_recursive(j,visited)
-
-
+            if j in visited:
+                pass
+            else:
+                self.dft_recursive(j, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -124,22 +133,30 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
+        print('Starting BFS')
         q = Queue()
+        visited = set()
         q.enqueue([starting_vertex])
-        
+        print(f'Starting vertex: {starting_vertex}')
+        print(f'End Room: {destination_vertex}')
+
         while destination_vertex not in q.queue[0]:
-          current_point = q.queue[0][-1]
-          joins = self.vertices[current_point]
-          for j in joins:
-            _ = [x for x in q.queue[0]]
-            _.append(j)
-            q.enqueue(_)
-          q.dequeue()
+            # while q.queue[0][-1] != destination_vertex:
+            # print(q)
+            current_point = q.queue[0][-1]
+            # print(f'current point: {current_point}')
+            joins = self.vertices[current_point].values()
+            # print(joins)
+            for j in joins:
+                # print(f'J: {j}')
+                if j != '?' and j not in visited:
+                    visited.add(j)
+                    _ = [x for x in q.queue[0]]
+                    _.append(j)
+                    q.enqueue(_)
+            q.dequeue()
 
         return q.queue[0]
-
-          
-
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -149,21 +166,21 @@ class Graph:
         """
         s = Stack()
         s.push([starting_vertex])
-        
+
         while destination_vertex not in s.stack[-1]:
-          current_point = s.stack[-1][-1]
-          
-          joins = self.vertices[current_point]
-          if joins is None:
-            s.pop()
-          else:
-            temp_list = []
-            for j in joins:
-              _ = [x for x in s.stack[-1]]
-              _.append(j)
-              temp_list.append(_)
-            for tl in temp_list:
-              s.push(tl)
-          #s.pop()
+            current_point = s.stack[-1][-1]
+
+            joins = self.vertices[current_point]
+            if joins is None:
+                s.pop()
+            else:
+                temp_list = []
+                for j in joins:
+                    _ = [x for x in s.stack[-1]]
+                    _.append(j)
+                    temp_list.append(_)
+                for tl in temp_list:
+                    s.push(tl)
+            # s.pop()
 
         return s.stack[-1]
--- explore.py
+++ explore.py
@@ -1,8 +1,11 @@
 from utils import *
 
 mappy = mapper()
-# mappy.accumulate=False   #set pick up items to false
-# mappy.import_text_map = False
+mappy.accumulate = True  # set pick up items to false
+mappy.import_text_map = True
 mappy.create_starting_map()
+# mappy.go_to_room(1)
+mappy.get_treasure()
+# mappy.get_info()
 
-mappy.explore_random(450)
+# mappy.explore_random(118)
