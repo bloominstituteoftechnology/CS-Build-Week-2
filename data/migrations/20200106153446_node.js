5e2cf6d9eb518c10324171f1d0b0245573f4c366
cecil john tantay <cjbtantay@gmail.com>
2020-01-06 23:07:12 -0800

    start server

---
+++ data/migrations/20200106153446_node.js
@@ -0,0 +1,28 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('node', tbl => {
+    tbl.increments('id');
+    tbl.integer('room_id');
+    tbl.string('title');
+    tbl.string('description');
+    tbl.string('coordinates');
+    tbl.json('exits');
+    tbl.float('cooldown');
+    tbl.json('errors');
+    tbl.json('messages');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('node');
+};
+
+/**
+ *   "room_id": 0,
+  "title": "A Dark Room",
+  "description": "You cannot see anything.",
+  "coordinates": "(60,60)",
+  "exits": ["n", "s", "e", "w"],
+  "cooldown": 1.0,
+  "errors": [],
+  "messages": []
+ */
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(cors());
+
+server.use('/', (req, res) => {
+  res.send(`
+    <h1>Server Works</h1>
+  `);
+});
+
+module.exports = server;
