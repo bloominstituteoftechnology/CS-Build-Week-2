9485e9b9c9e5bd80d2707f2afadfa13eefe23668
jackcornellober <jackcornellober@gmail.com>
2020-02-04 10:18:28 -0800

    working on traversal

--- src/explore_world.py
+++ src/explore_world.py
@@ -14,28 +14,33 @@ world_graph = Graph()
 
 explored_rooms = set()
 
-last_room = 0
+last_room_id = 0
 
-world_graph.add_vertex(0,  "A brightly lit room",  "You are standing in the center of a brightly lit room. You notice a shop to the west and exits to the north, south and east.",  "(60,60)",  0,  "NORMAL",  [],  ['?','?','?','?'], ["You have walked south."])
+world_graph.add_vertex(0,  "A brightly lit room",  "You are standing in the center of a brightly lit room. You notice a shop to the west and exits to the north, south and east.",  "(60,60)",  0,  "NORMAL",  [],  {'n':'?','e':'?','s':'?','w':'?'}, ["You have walked south."])
 
-# while len(explored_rooms) < 500:
-    
-#     direction = response[""]
+{explored_rooms.add(0)}
 
-#     data = {"direction":direction}
+dir_reverse = {"n":"s","s":"n","e":"w","w":"e"}
 
-#     response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
+while len(explored_rooms) < 500:
 
-#     exits = response["exits"]
+    data = {"direction":direction}
+    response = requests.post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', headers=headers, data=data)
+    exits = {}
+    for d in response["exits"]:
+        exits[d] = '?'
 
-#     if response["room_id"] not in explored_rooms:
-#         world_graph.add_vertex(response["room_id"],response["title"],response["description"],response["coordinates"],response["elevation"],response["terrain"],response["items"],exits,response["messages"])
+    exits[dir_reverse[direction]] = last_room_id
+    world_graph.vertices[last_room_id]["exits"][direction] = response["room_id"]
 
-#     cooldown = response["cooldown"]
+    if response["room_id"] not in explored_rooms:
+        world_graph.add_vertex(response["room_id"],response["title"],response["description"],response["coordinates"],response["elevation"],response["terrain"],response["items"],exits,response["messages"])
 
-#     r = Timer(cooldown + 1, add_explored, (explored_rooms,response["room_id"]))
+    cooldown = response["cooldown"]
 
-#     last_room = response["room_id"]
+    r = Timer(cooldown + 1, add_explored, (explored_rooms,response["room_id"]))
+
+    last_room_id = response["room_id"]
 
 # Once done looping, add everything to database
 
