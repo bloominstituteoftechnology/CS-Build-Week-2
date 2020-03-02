46b0bce9bf03542b1e394858f7226241af7bd9d2
[Brandon] <desselle.brandon2@gmail.com>
2020-03-02 13:54:54 -0600

    Update functions.py

--- TreasureHunt/map/functions.py
+++ TreasureHunt/map/functions.py
@@ -2,6 +2,7 @@ from decouple import config
 import requests
 import time
 import sys
+import re
 
 
 def init_player():
@@ -48,3 +49,119 @@ def move_player(direction):
             else:
                 print(exception)
                 sys.exit(1)
+
+
+def move_wise(direction, room_id):
+    pass
+
+
+def take_item(item):
+    try:
+        item = requests.post(
+            f'{config('API_URL')}/api/adv/take/',
+            headers={
+                'Authorization': f'Token {config('API_KEY')}',
+                'Content-Type': 'application/json'
+            },
+            data={
+                'name': f'{item}'
+            }
+        )
+        return item
+    # if cooldown applies, add exception and while loop
+    except requests.exceptions.RequestExceptions as exception:
+        print(exception)
+        sys.exit(1)
+
+
+def drop_item(item):
+    try:
+        item = requests.post(
+            f'{config('API_URL')}/api/adv/drop/',
+            headers={
+                'Authorization': f'Token {config('API_KEY')}',
+                'Content-Type': 'application/json'
+            },
+            data={
+                'name': f'{item}'
+            }
+        )
+        return item
+    # if cooldown applies, add exception and while loop
+    except requests.exceptions.RequestExceptions as exception:
+        print(exception)
+        sys.exit(1)
+
+
+def sell_treasure():
+    while True:
+        try:
+            sale = requests.post(
+                f'{config('API_URL')}/api/adv/sell/',
+                headers={
+                    'Authorization': f'Token {config('API_KEY')}',
+                    'Content-Type': 'application/json'
+                },
+                data={
+                    'name': 'treasure'
+                }
+            )
+            message = sale['messages']
+            cooldown = sale['cooldown']
+            time.sleep(cooldown)
+            # returns a message and requires confirmation to sell
+            edited_message = re.sub('\([^)]*\)',
+                                    '(choose YES or NO.)', message)
+            print(edited_message)
+            prompt = input().lower().strip()
+            if promt == 'yes' or if prompt == 'y':
+                # make call to confirm
+                return confirm_treasure()
+            elif prompt == 'no' or if prompt == 'n':
+                # print confirmation message and return
+                # need to decide how to handle return within algorithm
+                print("Sale declined.")
+                return
+        # if cooldown applies, add exception and while loop
+        except requests.exceptions.RequestException as exception:
+            print(exception)
+            sys.exit(1)
+
+
+def confirm_treasure():
+    try:
+        confirm = requests.post(
+            f'{config('API_URL')}/api/adv/sell/',
+            headers={
+                'Authorization': f'Token {config('API_KEY')}',
+                'Content-Type': 'application/json'
+            },
+            data={
+                'name': 'treasure',
+                'confirm': 'yes'
+            }
+        )
+        cooldown = confirm['cooldown']
+        time.sleep(cooldown)
+        return confirm
+    except requests.exceptions.RequestException as exception:
+        print(exception)
+        sys.exit(1)
+
+
+def check_status():
+    try:
+        status = requests.post(
+            f'{config('API_URL')}/api/adv/status/',
+            headers={
+                'Authorization': f'Token {config('API_KEY')}',
+                'Content-Type': 'application/json'
+            }
+        )
+        cooldown = status['cooldown']
+        time.sleep(cooldown)
+        return status
+    # if cooldown applies, add exception and while loop
+    except requests.exceptions.RequestException as exception:
+        print(exception)
+        sys.exit(1)
