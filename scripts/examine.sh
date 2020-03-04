fbe1c4a785fc50b4c619d4e1e793e6470c3036fc
[Brandon] <desselle.brandon2@gmail.com>
2020-03-04 16:46:55 -0600

    Create examine.sh

---
+++ scripts/examine.sh
@@ -0,0 +1,2 @@
+curl -X POST -H 'Authorization: Token 385d50b06a06032df5af75fc0546ee49433c2578' -H "Content-Type: application/json" -d '{"name":"Well"}' https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/
+# {"name":"[NAME OF ITEM OR PLAYER]"}
