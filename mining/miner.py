f56b6c8f02bd1dcb05ff2917422b6b9b374476e9
[Brandon] <desselle.brandon2@gmail.com>
2020-03-04 11:42:02 -0600

    Trying a different approach

---
+++ mining/miner.py
@@ -0,0 +1,91 @@
+import hashlib
+import requests
+import time
+import sys
+import random
+
+from decouple import config
+from uuid import uuid4
+from timeit import default_timer as timer
+
+
+def proof_of_work(last_proof, diff):
+    """
+    Multi-Ouroboros of Work Algorithm
+    - Find a number p' such that the last six digits of hash(p) are equal
+    to the first six digits of hash(p')
+    - IE:  last_hash: ...AE9123456, new hash 123456888...
+    - p is the previous proof, and p' is the new proof
+    - Use the same method to generate SHA-256 hashes as the examples in class
+    """
+
+    start = timer()
+
+    print("Searching for next proof")
+    proof = 0
+    last = f'{last_proof}'.encode()
+    last_hash = hashlib.sha256(last).hexdigest()
+
+    while valid_proof(last_hash, last_proof, proof, diff) is False:
+        proof += 1
+
+    print("Proof found: " + str(proof) + " in " + str(timer() - start))
+    return proof
+
+
+def valid_proof(last_hash, last_proof, proof, diff):
+    """
+    Validates the Proof:  Multi-ouroborus:  Do the last six characters of
+    the hash of the last proof match the first six characters of the hash
+    of the new proof?
+    IE:  last_hash: ...AE9123456, new hash 123456E88...
+    """
+
+    guess = f'{last_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    if guess_hash[:diff] == '0'*diff:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        node = sys.argv[1]
+    else:
+        node = "https://lambda-treasure-hunt.herokuapp.com/api/bc"
+        headers = {"Authorization": config('API_KEY')}
+
+    coins_mined = 0
+
+    # Load or create ID
+    # f = open("my_id.txt", "r")
+    # id = f.read()
+    # print("ID is", id)
+    # f.close()
+
+    # if id == 'NONAME\n':
+    #     print("ERROR: You must change your name in `my_id.txt`!")
+    #     exit()
+    # Run forever until interrupted
+    while True:
+        # Get the last proof from the server
+        r = requests.get(url=node + "/last_proof", headers=headers)
+        last_data = r.json()
+        new_proof = proof_of_work(last_data['proof'], last_data['difficulty'])
+        print(f'New Proof {new_proof}')
+
+        post_data = {"proof": new_proof}
+        print(post_data)
+
+        r = requests.post(url=node + "/mine", json=post_data, headers=headers)
+        data = r.json()
+        print(data)
+        time.sleep(data["cooldown"])
+        # print(data['message'])
+        # if data['message'] == 'New Block Forged':
+        #     coins_mined += 1
+        #     print("Total coins mined: " + str(coins_mined))
+        # else:
+        #     print(data['message'])
---
+++ traversal/movement.py
@@ -0,0 +1,132 @@
+import hashlib
+import requests
+import time
+import sys
+import json
+from decouple import config
+from visited_rooms import visited_rooms
+
+
+# Import visited_rooms for wise movement
+# Traversal that uses DFS, to find path
+# Store is at room id 1
+store = 1
+# Name Changer is at 467
+name_change = 467
+# Well is at 55
+well = 55
+# Shrines is at 461, 374
+shrine1 = 461
+shrine2 = 374
+# Mine location (decoded coordinates as room id)
+# Add power abilites (STRETCH)
+
+node = "https://lambda-treasure-hunt.herokuapp.com/api"
+headers = {"Authorization": config('API_KEY')}
+
+# def wise_map(visited):
+#     # Get the current room information
+#     r = requests.get(url=node + "/adv/init", headers=headers)
+#     curr = r.json()
+#     print(curr)
+#     # Pass in the direction being moved
+#     # Grab the room_id of room going into
+#     # Run moving function
+
+player_data = requests.post(url=node + "/adv/status", headers=headers)
+player = player_data.json()
+
+
+def sell(player):
+    if len(player['inventory']) > 0:
+        data = {"name": "treasure"}
+        r = requests.post(url=node + "/adv/sell",
+                          json=data, headers=headers)
+        # Handle non-json response
+        print("data", data)
+        try:
+            print("cooldown", r.json()["cooldown"])
+            time.sleep(r.json()["cooldown"])
+            data = {"name": "treasure", "confirm": "yes"}
+            r = requests.post(url=node + "/adv/sell",
+                              json=data, headers=headers)
+            try:
+                print("cooldown", r.json()["cooldown"])
+                time.sleep(r.json()["cooldown"])
+                data = {"name": "treasure", "confirm": "yes"}
+                r = requests.post(url=node + "/adv/sell",
+                                  json=data, headers=headers)
+            except ValueError:
+                print("Error:  Non-json response")
+                print("next_room_id Response returned:")
+                print(r)
+        except ValueError:
+            print("Error:  Non-json response")
+            print("next_room_id Response returned:")
+            print(r)
+            sell(player)
+    else:
+        print('No more to sell!')
+
+
+def moving_function(traversal_path, rooms_id_list=None):
+    print(traversal_path, rooms_id_list)
+    for i in range(len(traversal_path)):
+        # data = {"direction": i, "next_room_id": str(room_id)}
+        data = {"direction": traversal_path[i],
+                "next_room_id": str(rooms_id_list[i])}
+        r = requests.post(url=node + "/adv/move",
+                          json=data, headers=headers)
+        # Handle non-json response
+        print("data", data)
+        try:
+            print("cooldown", r.json()["cooldown"])
+            time.sleep(r.json()["cooldown"])
+        except ValueError:
+            print("Error:  Non-json response")
+            print("next_room_id Response returned:")
+            print(r)
+
+
+r = requests.get(url=node + "/adv/init", headers=headers)
+time.sleep(r.json()["cooldown"])
+starting_room = r.json()
+
+
+def room_search(visited_rooms, starting_room, target):
+    queue = []
+    visited = set()
+    start = starting_room['room_id']
+    print("start", start)
+    queue.append(start)
+    rooms_id_list = [[]]
+    paths = [[]]
+    while len(queue) > 0:
+        path = paths.pop(0)
+        rooms_id = rooms_id_list.pop(0)
+        last_room = queue.pop(0)
+        print(f'Path {path}')
+
+        if last_room == target:
+            print(path)
+            print("last_room", last_room)
+            return moving_function(path, rooms_id)
+
+        else:
+            if last_room not in visited:
+                visited.add(last_room)
+                for d in visited_rooms[str(last_room)]:
+                    if visited_rooms[str(last_room)][d] not in visited:
+                        new_path = path.copy()
+                        new_path.append(d)
+                        new_rooms_id = rooms_id.copy()
+                        new_rooms_id.append(visited_rooms[str(last_room)][d])
+                        rooms_id_list.append(new_rooms_id)
+                        print(f'new Path {new_path}')
+                        paths.append(new_path)
+                        queue.append(visited_rooms[str(last_room)][d])
+            else:
+                pass
+
+
+room_search(visited_rooms, starting_room, name_change)
