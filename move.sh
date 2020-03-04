e06e0ac1cf2e23e6b1250914e77aa8e5c7d42e31
[Brandon] <desselle.brandon2@gmail.com>
2020-03-04 16:46:49 -0600

    Delete move.sh

--- move.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST -H 'Authorization: Token f976aafe27b40a76a602b84c15df748b3c6d7403' -H "Content-Type: application/json" -d '{"direction": "'"$1"'"}' https://lambda-treasure-hunt.herokuapp.com/api/adv/move/
