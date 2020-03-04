992274b57e495fb83410c542207c6271af35ad38
zoverlvx <zoverhulser@gmail.com>
2020-03-04 15:27:52 -0500

    Moves to room specified by direction.

---
+++ move.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Authorization: Token f976aafe27b40a76a602b84c15df748b3c6d7403' -H "Content-Type: application/json" -d '{"direction": "'"$1"'"}' https://lambda-treasure-hunt.herokuapp.com/api/adv/move/
