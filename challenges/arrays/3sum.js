3945464595325522a7e7b2975a2e2f23a2bd66fe
umekow <umekolashun@gmail.com>
2020-05-05 18:37:39 -0500

    javascript 3sum

---
+++ challenges/arrays/3sum.js
@@ -0,0 +1,26 @@
+function threeNumberSum(array, targetSum) {
+    array.sort((a, b) => a - b)
+    let triplets = []
+  
+    for( i = 0; i < array.length - 2; i++){
+        let left = i + 1
+        let right = array.length - 1
+        while (left < right){
+            let current_sum = array[i] + array[left] + array[right]
+            if (current_sum === targetSum){
+                triplets.push([array[i], array[left], array[right]])
+                left++ 
+                right-- 
+            }
+            else if (current_sum < targetSum){
+                left++ 
+            }
+            else if (current_sum > targetSum){
+                right--
+            }
+              
+        }
+    }
+      
+    return triplets
+  }
