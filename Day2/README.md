1759b220711ca4a6af4b20a2b5873d1480c10635
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-08-25 13:30:57 -0700

    New challenge solutions, and implemented working naive binary tree creation function in balancedbinarytree problem.

--- Day2/README.md
+++ Day2/README.md
@@ -46,3 +46,67 @@ Expected Outputs:
 5. 58
 6. 1994
 ```
+
+## [Balance Binary Tree](./balancedbinarytree.py)
+
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+>a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example Inputs:
+
+``` python
+1. [3,9,20,None,None,15,7]
+
+>>    3
+>>   / \
+>>  9  20
+>>    /  \
+>>   15   7
+
+2. [1,2,2,3,3,None,None,4,4]
+
+>>       1
+>>      / \
+>>     2   2
+>>    / \
+>>   3   3
+>>  / \
+>> 4   4
+
+3. []
+```
+
+Expected Outputs:
+
+``` python
+1. True
+2. False
+3. True
+```
+
+## [Two Numbers](./twonumbers.py)
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example Inputs:
+
+```python
+1. [2, 7, 11, 15], 9
+2. [3,2,4], 6
+3. [2, 3, 6, 7], 13
+4. [1, 3, 8, 7, 9], 15
+```
+
+Expected Outputs:
+
+```python
+1. [0, 1]
+2. [1, 2]
+3. [2, 3]
+4. [2, 4]
+```
--- Day2/balancedbinarytree.py
+++ Day2/balancedbinarytree.py
@@ -1,12 +1,18 @@
 from queue import Queue
 
 # Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
+    def __repr__(self):
+        return f"({self.val} - {self.left}, {self.right})"
+
+
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
         def get_height(n: TreeNode) -> int:
@@ -16,25 +22,31 @@ class Solution:
             if n not in cache:
                 cache[n] = 1 + max(get_height(n.left), get_height(n.right))
             return cache[n]
-        
+
         if root is None:
             return True
         l_height = get_height(root.left)
         r_height = get_height(root.right)
-            
-        
+
         return abs(l_height - r_height) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
-    
+
+
 def make_tree(nums: list) -> TreeNode:
-    root = TreeNode(nums[0])
+    """
+    This implementation is very naive and breaks for complicated examples.
+    """
+    try:
+        root = TreeNode(nums[0])
+    except IndexError:
+        return TreeNode(None)
     nums = nums[1:]
     i = 0
     q = Queue()
     q.put(root)
     while i < len(nums) - 1:
         cur = q.get()
-        l = TreeNode(nums[i:])
-        r = TreeNode(nums[i + 1:])
+        l = TreeNode(nums[i]) if nums[i] != None else None
+        r = TreeNode(nums[i + 1]) if nums[i + 1] != None else None
         cur.left = l
         cur.right = r
         i += 2
@@ -42,15 +54,30 @@ def make_tree(nums: list) -> TreeNode:
         q.put(r)
     return root
 
+
 test = Solution()
 
-ex1 = [3,9,20,None,None,15,7]
-ex2 = [1,2,2,3,None,None,3,4,None,None,4]
+ex1 = [3, 9, 20, None, None, 15, 7]
+ex2 = [1, 2, 2, 3, 3, None, None, 4, 4]
+ex3 = []
+
+# this tree breaks my naive make_tree function implementation
+# eventually I will fix the function when I have time so that
+# any tree can be tested. After the fixes are implemented, this
+# should return False.
+ex4 = [1, 2, 2, 3, None, None, 3, 4, None, None, 4]
+
+
 t1 = make_tree(ex1)
 t2 = make_tree(ex2)
+t3 = make_tree(ex3)
+# t4 = make_tree(t4)
 sol1 = test.isBalanced(t1)
 sol2 = test.isBalanced(t2)
+sol3 = test.isBalanced(t3)
+# sol4 = test.isBalanced(t4)
 
 print(sol1)
 print(sol2)
-print(test.isBalanced(TreeNode(None)))
+print(sol3)
+# print(sol4)
