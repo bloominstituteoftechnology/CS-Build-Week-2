fb6e6097a76fb9200a32bb9d8250ef9006deef6e
wesley.moody <wgm3190@gmail.com>
2020-08-25 09:33:54 -0500

    contains dups in JS

---
+++ contains-dups.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  // create a set 
+  let numbers = new Set();
+  // iterate over the array
+  for (let num of nums) {
+    // get to a number, check if it's already in the set
+    if(!numbers.has(num)) {
+      // if it's not in the set add the number to the set
+      numbers.add(num);                
+      // but if you see it was already added to the set
+    } else {
+      // signal the array contains duplicates
+      return true;
+    }
+  }
+  // if code runs and no duplicates are found, return false
+  return false;           
+};
